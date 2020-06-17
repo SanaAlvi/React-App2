fe1453ed2b5d1d3576fcec539c78d48fda425971
SanaAlvi <sanaalvi066@gmail.com>
2020-06-17 12:29:53 +0500

    Update Child.js
    use as array {counterValue[0]} and give index 0

--- src/Child.js
+++ src/Child.js
@@ -2,14 +2,21 @@ import React, { useContext } from 'react';
 import './App.css';
 import CounterContext from './ConterContext';
 
-// import Parent from './Parent';
+// if we are using let [count,setCount] =useState(25) in App.js and we simply 
+// <button onClick={() => {counterValue++}}>Add</button>  do this in child 
+// value will never shows in page to be updated means update hu jahy ga render nai hu ga.
+
+
+// we have to give {counterValue[0]} beacuse it is array and first part have number
+// second have function in line 18
 
 const Child = () => {
     let counterValue = useContext (CounterContext)
     return(
         <div className="App-header">
             <h1>This is first Child </h1>
-           <h2> Counter Value is {counterValue} </h2>
+            <h2> Counter Value is {counterValue[0]} </h2> 
+            <button onClick={() => {counterValue[1](counterValue[0]++)}}>Change it</button>         
         </div>
     )
 }
