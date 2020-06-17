bf02243aa201ef3d9f923a45851e341971960518
SanaAlvi <sanaalvi066@gmail.com>
2020-06-17 13:29:20 +0500

    Update Parent.js

--- src/Parent.js
+++ src/Parent.js
@@ -1,18 +1,19 @@
 import React, { useContext } from 'react';
 import './App.css';
 import Child from './Child';
+import Child2 from './Child2';
 import Countercontext from './ConterContext';
 
 const Parent = () => {
     let CountState = useContext (Countercontext) 
     return(
-        <div className="App-header">
+        <div className="App">
             <Child />
             <h1>Hello from Parent:{CountState[0]}</h1>
             <button onClick={() => {CountState[1](2)}}>
                 Change it
             </button>
-           
+            <Child2 />
         </div>
     )
 }
