4480a4b59e9997821030efb6165107d051f2d350
SanaAlvi <sanaalvi066@gmail.com>
2020-06-17 13:58:01 +0500

    Update CounterReducer.js

--- src/CounterReducer.js
+++ src/CounterReducer.js
@@ -2,7 +2,9 @@ const CounterReducer = (state, action) => {
     switch (action) {
       case 'INCREMENT':
         return state + 1
+      case 'DECREMENT':
+        return state - 1
     }
   }
-  
+
   export default CounterReducer;
