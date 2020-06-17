db90bed14862f73e56e68212530574ee001bc130
SanaAlvi <sanaalvi066@gmail.com>
2020-06-17 13:57:54 +0500

    Update Child2.js

--- src/Child2.js
+++ src/Child2.js
@@ -2,13 +2,16 @@ import React, { useReducer } from 'react';
 import CounterReducer from './CounterReducer';
  
 const Child2 = () => {
-    let [state, dispatch] = useReducer(CounterReducer,24);
+    let [state, dispatch] = useReducer(CounterReducer,1);
     return(
         <div className="App">
             <h2>This is Child2 made for learning Reducer</h2>
             <h3>Value of Reducer: {state}</h3>
             <button onClick={() =>dispatch("INCREMENT") }>
                 Increment Reducer
+            </button>
+            <button onClick={() =>dispatch("DECREMENT") }>
+                Decrement Reducer
             </button>
         </div>
     ) 
