50d7d9eef6c0e4ed8d3ad87804d601de56ca62e8
SanaAlvi <sanaalvi066@gmail.com>
2020-06-17 12:52:39 +0500

    Update Parent.js
    change the position of calling child

--- src/Parent.js
+++ src/Parent.js
@@ -7,11 +7,12 @@ const Parent = () => {
     let CountState = useContext (Countercontext) 
     return(
         <div className="App-header">
+            <Child />
             <h1>Hello from Parent:{CountState[0]}</h1>
             <button onClick={() => {CountState[1](2)}}>
                 Change it
             </button>
-           <Child />
+           
         </div>
     )
 }
