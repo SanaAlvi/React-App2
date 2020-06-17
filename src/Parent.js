156923c8681f7274ed17ce742a11977a21c6044a
SanaAlvi <sanaalvi066@gmail.com>
2020-06-17 12:42:20 +0500

    Update Parent.js
    getting index 1 of array

--- src/Parent.js
+++ src/Parent.js
@@ -1,11 +1,16 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import './App.css';
 import Child from './Child';
+import Countercontext from './ConterContext';
 
-const Parent = (props) => {
+const Parent = () => {
+    let CountState = useContext (Countercontext) 
     return(
         <div className="App-header">
-            <h1>Hello from Parent</h1>
+            <h1>Hello from Parent:{CountState[0]}</h1>
+            <button onClick={() => {CountState[1][2]}}>
+                Change it
+            </button>
            <Child />
         </div>
     )
